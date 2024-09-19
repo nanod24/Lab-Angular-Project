54d6ef4b089056ef2598a5f71900ad623847ec96
diana890 <diananano@yahoo.com>
2024-09-18 22:54:30 -0400

    working on the student-list component

--- src/app/student-detail/student-detail.component.ts
+++ src/app/student-detail/student-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import {User} from "../Shared/Modules/user"
 
 @Component({
   selector: 'app-student-detail',
@@ -8,5 +9,7 @@ import { Component } from '@angular/core';
   styleUrl: './student-detail.component.scss'
 })
 export class StudentDetailComponent {
-
+  //Input decorator mark a property as input
+  // receive data from parent
+@Input() student?: User;
 }
